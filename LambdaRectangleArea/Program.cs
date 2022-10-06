12a391cf71c871e07028b807e58f598338b61af3
b1106857 <b1106857@TU39053>
2022-10-06 10:47:06 +0100

    Add project files.

---
+++ LambdaRectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// User input for the length and width of the rectangle
+Console.WriteLine("Please enter width of rectangle");
+int w = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Pleae enter lenght of rectangle");
+int l = Convert.ToInt32(Console.ReadLine());
+
+// Lambda expression 
+Func<int, int> recArea = (area) => w * l;
+
+// Console output
+int area = recArea(l * w);
+Console.WriteLine("Area of rectangle", area);
